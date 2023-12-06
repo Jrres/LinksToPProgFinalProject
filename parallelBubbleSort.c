9c31f9898df2cf096853f438e081623aed51623d
Jack <80206848+Jrres@users.noreply.github.com>
2023-12-06 17:25:18 -0500

    Update parallelBubbleSort.c

--- parallelBubbleSort.c
+++ parallelBubbleSort.c
@@ -3,90 +3,110 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-typedef struct {
-  int *arr;
-  int start;
-  int end;
+
+typedef struct {  //struct definition of a thread argument
+ int *arr;
+ int start;
+ int end;
 } thread_args_t;
 
-void swap(int *a, int *b) {
-  int temp = *a;
-  *a = *b;
-  *b = temp;
+
+void swap(int *a, int *b) { //helper function to switch 2 elements in an array
+ int temp = *a;
+ *a = *b;
+ *b = temp;
 }
 
-void *thread_sort(void *args) {
-  thread_args_t *data = (thread_args_t *) args;
-  int n = data->end - data->start;
-  bool swapped = true;
 
-  while (swapped) {
-    swapped = false;
-    for (int i = data->start; i < data->end - 1; ++i) {
-      if (data->arr[i] > data->arr[i + 1]) {
-        swap(&data->arr[i], &data->arr[i + 1]);
-        swapped = true;
-      }
-    }
-  }
+void *thread_sort(void *args) {  // implementation of bubble sort
+ thread_args_t *data = (thread_args_t *) args;   //store the data of each chunk
+ int n = data->end - data->start;  // range of values to loop through
+ bool swapped = true;  // conditional to check if values has swapped
+                       //if not then save time and break
+ while (swapped) {
+   swapped = false;
+   for (int i = data->start; i < data->end - 1; ++i) {
+     if (data->arr[i] > data->arr[i + 1]) {
+       swap(&data->arr[i], &data->arr[i + 1]); //perform the swapping algorithm
+       swapped = true;
+     }
+   }
+ }
 
-  pthread_exit(NULL);
+
+ pthread_exit(NULL);
 }
+
 
 void parallel_bubble_sort(int *arr, int n, int num_threads) {
-  pthread_t threads[num_threads];
-  thread_args_t thread_args[num_threads];
+ pthread_t threads[num_threads]; // init array of threads to store their ids
+ thread_args_t thread_args[num_threads]; //arr of args to pass to each thread if needed
 
-  int chunk_size = n / num_threads;
 
-  // Divide the array into chunks and create threads for each
-  for (int i = 0; i < num_threads; ++i) {
-    int start = i * chunk_size;
-    int end = (i == num_threads - 1) ? n : start + chunk_size;
+ int chunk_size = n / num_threads; //number of elements each thread takes in the array
 
-    thread_args[i].arr = arr;
-    thread_args[i].start = start;
-    thread_args[i].end = end;
 
-    pthread_create(&threads[i], NULL, thread_sort, &thread_args[i]);
-  }
+ // Divide the array into chunks and create threads for each
+ for (int i = 0; i < num_threads; ++i) {
+   int start = i * chunk_size; //start index of each thread's chunk
+   int end = (i == num_threads - 1) ? n : start + chunk_size; // end index of each threads chunk
+     //args to pass to each thread containing start, end, and array itself
+   thread_args[i].arr = arr;
+   thread_args[i].start = start;
+   thread_args[i].end = end;
+     //during thread creation. each thread will execute thread_sort <=> bubble sort with its chunk
+     //The function will also send the args and ids of each thread
+   pthread_create(&threads[i], NULL, thread_sort, &thread_args[i]);
+ }
 
-  // Wait for all threads to finish
-  for (int i = 0; i < num_threads; ++i) {
-    pthread_join(threads[i], NULL);
-  }
 
-  // Additional sorting steps if needed (e.g., checking for fully sorted array)
+   // Wait for all threads to finish
+ for (int i = 0; i < num_threads; ++i) {
+   pthread_join(threads[i], NULL);
+ }
+
+
+ // Additional sorting steps if needed (e.g., checking for fully sorted array)
 }
 
+
 int main() {
-  int n=20000;
-  int num_threads=1;
+ //  intialization
+  int n=20000; //  adjust input size as needed for the data table
 
-  int *arr = (int *)malloc(n * sizeof(int));
 
-  // Initialize the array with random values
-  srand(time(NULL));
-  for (int i = 0; i < n; ++i) {
-    arr[i] = rand() % 100;
-  }
+ int num_threads=1; //  also adjust number of threads here
 
-  printf("Unsorted array: ");
-  for (int i = 0; i < n; ++i) {
-    printf("%d ", arr[i]);
-  }
-  printf("\n");
 
-  // Sort the array using pthreads
-  parallel_bubble_sort(arr, n, num_threads);
+ int *arr = (int *)malloc(n * sizeof(int)); // dynamically assign size to arr
 
-  printf("Sorted array: ");
-  for (int i = 0; i < n; ++i) {
-    printf("%d ", arr[i]);
-  }
-  printf("\n");
 
-  free(arr);
+ // Initialize the array with random values
+ srand(time(NULL));
+ for (int i = 0; i < n; ++i) {
+   arr[i] = rand() % 100;
+ }
 
-  return 0;
+
+ printf("array before applying parallel bubble sort: ");
+ for (int i = 0; i < n; ++i) {
+   printf("%d ", arr[i]);
+ }
+ printf("\n");
+
+
+  parallel_bubble_sort(arr, n, num_threads);  // Sort the array using pthreads
+
+
+ printf("Sorted array after parallel bubble sort: ");
+ for (int i = 0; i < n; ++i) {
+   printf("%d ", arr[i]);
+ }
+ printf("\n");
+
+
+ free(arr);
+
+
+ return 0;
 }
